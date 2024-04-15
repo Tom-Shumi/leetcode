184a8feab3049db78f0f9c2980e2a2f72e446b68
Shumiya Tomohide <tshumiya281@gmail.com>
2024-04-15 22:23:44 +0900

    88 done

--- 88/MergeSortedArray.java
+++ 88/MergeSortedArray.java
@@ -1,5 +1,6 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
         if (nums2.length == 0) {
             return;
         }    
--- 88/README.md
+++ 88/README.md
@@ -1,1 +1,5 @@
 https://leetcode.com/problems/merge-sorted-array/description/
+
+## memo
+
+- I want to solve more memory efficiently.
