e0fcfe8e9df5869601abae11ced67db2488ed8a0
Shumiya Tomohide <tshumiya281@gmail.com>
2024-04-21 20:38:26 +0900

    14 done14 done14 done14 done14 done14 done14 done14 done14 done14 done14 done14 done14 done14 done

---
+++ 14/LongestCommonPrefix.java
@@ -0,0 +1,29 @@
+class Solution {
+  public String longestCommonPrefix(String[] strs) {
+    String result = strs[0];
+    if (result.equals("")) {
+      return "";
+    }
+
+    for (int i = 1; i < strs.length; i++) {
+      String temp = "";
+      for (int j = 0; j < strs[i].length(); j++) {
+        if (result.length() == j) {
+          break;
+        }
+
+        if (result.charAt(j) == strs[i].charAt(j)) {
+          temp = temp + result.charAt(j);
+        } else {
+          break;
+        }
+      }
+      result = temp;
+      if (temp.equals("")) {
+        break;
+      }
+    }
+
+    return result;
+  }
+}
